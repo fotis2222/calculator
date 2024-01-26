9e9881227ce1e75efafa18ade164f21e312e13bf
fotis2222 <nitsas2012@gmail.com>
2024-01-26 20:08:40 +0200

    added converter

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("(1) claculator\nconversions coming soon...");
+        System.out.println("(1) calculator\n(2) converter\nconversions coming soon...");
         int mode = sc.nextInt();
         if (mode == 1) {
             System.out.println("operation:\n(1) add\n(2) subtract\n(3) multiply\n(4) divide");
@@ -39,6 +39,24 @@ public class Calculator {
                 int b = sc.nextInt();
                 sc.nextLine();
                 System.out.println("result: " + (a / b));
+                exit();
+            } else {
+                explain();
+            }
+        } else if (mode == 2) {
+            System.out.println("(1) kg to lbs\n(2) lbs to kgs");
+            int conversion = sc.nextInt();
+            if (conversion == 1) {
+                System.out.println("kg:");
+                double kg = sc.nextDouble();
+                System.out.println((kg * 2.2) + " lbs");
+                sc.nextLine();
+                exit();
+            } else if (conversion == 2) {
+                System.out.println("lbs:");
+                double lbs = sc.nextDouble();
+                System.out.println((lbs / 2.2) + " kg");
+                sc.nextLine();
                 exit();
             } else {
                 explain();
